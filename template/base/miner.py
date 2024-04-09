0fe19718a12ed7461649df8e06cd4d7824b04ac5
gitphantomman <minatolegend0702@outlook.com>
2024-04-09 16:05:30 +0000

    remove axon attach

--- template/base/miner.py
+++ template/base/miner.py
@@ -61,12 +61,12 @@ class BaseMinerNeuron(BaseNeuron):
         self.axon = bt.axon(wallet=self.wallet, config=self.config)
 
         # Attach determiners which functions are called when servicing a request.
-        bt.logging.info(f"Attaching forward function to miner axon.")
-        self.axon.attach(
-            forward_fn=self.forward,
-            blacklist_fn=self.blacklist,
-            priority_fn=self.priority,
-        )
+        # bt.logging.info(f"Attaching forward function to miner axon.")
+        # self.axon.attach(
+        #     forward_fn=self.forward,
+        #     blacklist_fn=self.blacklist,
+        #     priority_fn=self.priority,
+        # )
         bt.logging.info(f"Axon created: {self.axon}")
 
         # Instantiate runners
